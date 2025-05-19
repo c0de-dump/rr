26c662a06284b78fb5779781879a4cb218e4630c
m3n0l1f3 <ma4thx@gmail.com>
2025-05-19 07:59:39 -0700

    rails tutorial: rich text & file upload

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,8 @@
+# frozen_string_literal: true
+
+# Rails tutorial example
 class Product < ApplicationRecord
+  has_one_attached :featured_image
+  has_rich_text :description
   validates :name, presence: true
 end
